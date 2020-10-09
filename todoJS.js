b1b0d112ae40070984d84a99a2906e89949b4e04
Your name here <benmc1231@gmail.com>
2020-10-09 14:06:15 +1000

    Add to list functionality complete
    Changed table to a list, added CSS.
    Working on delete button functionality

--- todoJS.js
+++ todoJS.js
@@ -8,8 +8,49 @@ function currentDate(){
     document.getElementById("date").innerHTML ="Todays date: " + currentDay;
 }
 
-
+//get current date on page loading
 window.onload = function() {
     currentDate();
-  }
+}
+
+function addItem(){
+    var newListItem = document.createElement("li");
+    var textForNewItem = document.getElementById("myInput").value;
+    var newListText = document.createTextNode(textForNewItem);
+
+    newListItem.appendChild(newListText)
+    if (textForNewItem == ""){
+        document.getElementById("myInput").placeholder = "You must enter something";
+    }
+    else {
+        document.getElementById("myList").appendChild(newListItem);
+    }
+    
+    //reset text bar
+    document.getElementById("myInput").value = "";
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "deleteButton";
+    span.appendChild(txt);
+    li.appendChild(span);
+  
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+}
+
+// Create a close button for each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "deleteButton";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
 
