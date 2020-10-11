6401033d0453489611f1532bdfdbce980368c116
Your name here <benmc1231@gmail.com>
2020-10-11 15:04:40 +1000

    Completed todo list
    Button addition and functionality working

--- todoJS.js
+++ todoJS.js
@@ -8,49 +8,65 @@ function currentDate(){
     document.getElementById("date").innerHTML ="Todays date: " + currentDay;
 }
 
-//get current date on page loading
-window.onload = function() {
-    currentDate();
-}
+// Create a delete button for each list item
+function addDeleteButtons(){
+  var myList = document.getElementsByTagName("LI");
+  var i;
 
-function addItem(){
-    var newListItem = document.createElement("li");
-    var textForNewItem = document.getElementById("myInput").value;
-    var newListText = document.createTextNode(textForNewItem);
-
-    newListItem.appendChild(newListText)
-    if (textForNewItem == ""){
-        document.getElementById("myInput").placeholder = "You must enter something";
-    }
-    else {
-        document.getElementById("myList").appendChild(newListItem);
-    }
-    
-    //reset text bar
-    document.getElementById("myInput").value = "";
-
+  for (i = 0; i < myList.length; i++) {
     var span = document.createElement("SPAN");
     var txt = document.createTextNode("\u00D7");
     span.className = "deleteButton";
     span.appendChild(txt);
-    li.appendChild(span);
-  
-    for (i = 0; i < close.length; i++) {
-      close[i].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
-      }
-    }
+    myList[i].appendChild(span);
+  }
 }
 
-// Create a close button for each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
+//give functionality to the delete buttons
+function clickToDeleteButton(){
+  var deleteButtons = document.getElementsByClassName("deleteButton");
+  var i;
+  for (i = 0; i < deleteButtons.length; i++) {
+    deleteButtons[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+//get current date on page loading along with the placeholder list buttons
+window.onload = function() {
+    currentDate();
+    addDeleteButtons();
+    clickToDeleteButton();
+}
+
+//add new list item
+function addItem(){
+  var newListItem = document.createElement("li");
+  var textForNewItem = document.getElementById("myInput").value;
+  var newListText = document.createTextNode(textForNewItem);
+
+  newListItem.appendChild(newListText)
+  if (textForNewItem == ""){
+      document.getElementById("myInput").placeholder = "You must enter something";
+  }
+  else {
+      document.getElementById("myList").appendChild(newListItem);
+  }
+  
+  //reset text bar
+  document.getElementById("myInput").value = "";
+
+
+  // Create a delete button
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
   span.className = "deleteButton";
   span.appendChild(txt);
-  myNodelist[i].appendChild(span);
+  newListItem.appendChild(span);
+  
+  clickToDeleteButton();
 }
+
 
